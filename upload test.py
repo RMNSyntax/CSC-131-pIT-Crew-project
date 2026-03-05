2df334dde17457b3b9180c7e15234fad23a4c3d8
RMNSyntax <rmnichols@csus.edu>
2026-03-04 19:19:15 -0800

    refactored upload test slightly, removed the credentials file because google API automatically disables the authentication key if it's detected in a repository, even a semi-private one like ours. just let me know if you need help setting up the authentication for the spreadsheet we'll be working on

--- upload test.py
+++ upload test.py
@@ -12,4 +12,9 @@ client = gspread.authorize(credentials)
 
 sheet = client.open('python data input test').sheet1
 
+# Updating the first column, second row (A2) with a given string
 sheet.update([['Hello world!! this is the second row of the first column!']], 'A2')
+
+# Getting the contents of the very first cell in the spreadsheet and printing it to the console window
+first_cell = sheet.cell(1,1)
+print(f"First cell of Data: {first_cell}")
